3b9e8f3c5be20db670e400016342dcbb15a9ee31
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-25 00:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const parseCode = (str) => {
   // your code here
+	 let arr=str.split("0");
+  // console.log(arr);
+  let a1=[];
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]!=""){
+      a1.push(arr[i]);
+    }
+  }
+   // console.log(a1);
+  let obj={
+    firstname:a1[0],
+    lastname:a1[1],
+    id:a1[2]
+
+
+  }
+  return obj;
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
